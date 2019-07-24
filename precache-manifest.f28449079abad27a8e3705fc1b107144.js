fb73db1fd669f543484819ef6cca81f514f90401
Anton Azhhirevych <azhhirevych943@gmail.com>
2019-07-24 16:09:02 +0300

    Updates

---
+++ precache-manifest.f28449079abad27a8e3705fc1b107144.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "474f0f3945db3024f58295d6b2bdc448",
+    "url": "/goit-react-hw-02-movies/index.html"
+  },
+  {
+    "revision": "4d47e70d12a2df0c1be7",
+    "url": "/goit-react-hw-02-movies/static/css/main.0bf3c5ee.chunk.css"
+  },
+  {
+    "revision": "b7e94455b40ccb98428a",
+    "url": "/goit-react-hw-02-movies/static/js/2.c9583138.chunk.js"
+  },
+  {
+    "revision": "4d47e70d12a2df0c1be7",
+    "url": "/goit-react-hw-02-movies/static/js/main.d6997cac.chunk.js"
+  },
+  {
+    "revision": "c0f0bf3555f23a72488d",
+    "url": "/goit-react-hw-02-movies/static/js/runtime~main.4f93631a.js"
+  }
+]);
